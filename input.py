21209f82c6e22e18ccca6bca085df4f6d352a183
Piotr Bednarz (UST <U58221@ust-global.com>
2017-07-21 15:17:22 +0200

    reaplacing None by None

--- input.py
+++ input.py
@@ -15,12 +15,12 @@ def read_file():
                 l[3] = 1000
             if l[1] not in "S0" and l[3] != 1000:
                 l[3] = int(l[3])
-                l.append("None")
+                l.append(None)
             elif l[1] in "S0" and l[3] != 1000:
                 l[3] = int(l[3])
                 l[4] = int(l[4])
             elif l[3] == 1000:
-                l.append("None")
+                l.append(None)
             data.append(l)
     return data
 
