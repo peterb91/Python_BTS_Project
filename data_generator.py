7a8dee46e0dbbc8f86c6ec89bbf6dbce8efefe93
arisa94 <arisa9404@gmail.com>
2017-07-21 10:14:26 +0200

    Little changes according to PEP8

--- data_generator.py
+++ data_generator.py
@@ -1,13 +1,14 @@
 import random
 
-def randomData (amount):
+
+def random_data(amount):
     with open("dataset.txt", "w+") as file:
         for i in range(amount):
             links = ['UL', 'DL', 'SL']
             col1 = random.choice(links)
-            types = ['S0', 'S0', 'S0', 'N1', 'N2', 'N3']
+            types = ['S0', 'S0', 'S0', 'N1', 'N2', 'N3', "N4", "N5", "N6"]
             col2 = random.choice(types)
-            stations = ['MS776', 'MS222']
+            stations = ['MS776', 'MS222', "nuishui", "6784jk"]
             col3 = random.choice(stations)
             strength = [i for i in range(-99, -10, 1)] + ["missing"]
             col4 = random.choice(strength)
@@ -17,4 +18,4 @@ def randomData (amount):
                 col5 = ""
             file.write("%s %s %s %s %s\n" %(col1, col2, col3, col4, col5))
 
-randomData(100000)
+random_data(100000)
