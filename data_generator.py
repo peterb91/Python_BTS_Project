b16876596cacad06f0bfe15e7ce3df17a28a4460
Piotr Bednarz (UST <U58221@ust-global.com>
2017-07-21 11:51:32 +0200

    changing data generator

--- data_generator.py
+++ data_generator.py
@@ -1,18 +1,17 @@
 import random
 
-
-def random_data(amount):
+def random_data (amount):
     with open("dataset.txt", "w+") as file:
         for i in range(amount):
-            links = ['UL', 'DL', 'SL']
+            links = ["UL", "DL", "SL"]
             col1 = random.choice(links)
-            types = ['S0', 'S0', 'S0', 'N1', 'N2', 'N3', "N4", "N5", "N6"]
-            col2 = random.choice(types)
-            stations = ['MS776', 'MS222', "nuishui", "6784jk"]
+            terminals = ["S0", "S0", "S0", "N1", "N2", "N3", "N4", "N5", "N6"]
+            col2 = random.choice(terminals)
+            stations = ["MS776", "MS222", "MS543", "MS754"]
             col3 = random.choice(stations)
-            strength = [i for i in range(-99, -10, 1)] + ["missing"]
+            strength = [i for i in range(-99, -95, 2)] + [i for i in range(-95, -45, 1)] + [i for i in range(-45, -10, 8)] + ["missing"]
             col4 = random.choice(strength)
-            if (col2 in 'S0') and (str(col4) not in 'missing'):
+            if col2 in "S0" and str(col4) not in "missing":
                 col5 = random.randint(1,5)
             else:
                 col5 = ""
