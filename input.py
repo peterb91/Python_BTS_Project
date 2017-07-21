a97386e90e466fc2ba79401e8867d9e12713364b
Piotr Bednarz (UST <U58221@ust-global.com>
2017-07-21 13:37:41 +0200

    std input implementation

--- input.py
+++ input.py
@@ -1,29 +1,27 @@
+import sys
 
 def read_file():
 
     sep = " "
     data = []
 
-    with open("dataset.txt") as file:
-        content = file.readlines()
-    for line in content:
+    file = sys.stdin
+    for line in file:
         line = line.strip()
-        if line[:2] in ('DL', 'UL'):
-            l = line.split(sep)
-            if l[1] not in 'S0' and l[3] not in 'missing':
-                l[3] = int(l[3])
-                l.append('None')
-            elif l[1] in 'S0' and l[3] not in 'missing':
-                l[3] = int(l[3])
-                l[4] = int(l[4])
-            elif l[3] in 'missing':
-                l.append('None')
-            #print(l)
-            data.append(l)
-        #else:
-            #l = [incorrect, incorrect, incorrect, incorrect, incorrect]
-            #data.append(l)'''
+        l = line.split(sep)
+        if l[3] in "missing":
+            l[3] = 1000
+        if l[1] not in "S0" and l[3] != 1000:
+            l[3] = int(l[3])
+            l.append("None")
+        elif l[1] in "S0" and l[3] != 1000:
+            l[3] = int(l[3])
+            l[4] = int(l[4])
+        elif l[3] == 1000:
+            l.append("None")
+        print(l)
+        data.append(l)
     return data
-    #print(data)
+    print(data)
 
 read_file()
