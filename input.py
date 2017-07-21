91dfca7294e605d68d2c26ba3316f265a2ca1fa8
Piotr Bednarz (UST <U58221@ust-global.com>
2017-07-21 14:31:35 +0200

    commented std in, missing change to 100

--- input.py
+++ input.py
@@ -1,4 +1,4 @@
-import sys
+#import sys
 import database
 
 def read_file():
@@ -6,20 +6,22 @@ def read_file():
     sep = " "
     data = []
 
-    file = sys.stdin
-    for line in file:
-        line = line.strip()
-        l = line.split(sep)
-        if l[3] in "missing":
-            l[3] = 1000
-        if l[1] not in "S0" and l[3] != 1000:
-            l[3] = int(l[3])
-            l.append("None")
-        elif l[1] in "S0" and l[3] != 1000:
-            l[3] = int(l[3])
-            l[4] = int(l[4])
-        elif l[3] == 1000:
-            l.append("None")
+    #file = sys.stdin
+    with open("dataset.txt") as file:
+        content = file.readlines()
+        for line in content:
+            line = line.strip()
+            l = line.split(sep)
+            if l[3] in "missing":
+                l[3] = 1000
+            if l[1] not in "S0" and l[3] != 1000:
+                l[3] = int(l[3])
+                l.append("None")
+            elif l[1] in "S0" and l[3] != 1000:
+                l[3] = int(l[3])
+                l[4] = int(l[4])
+            elif l[3] == 1000:
+                l.append("None")
         print(l)
         data.append(l)
     return data
