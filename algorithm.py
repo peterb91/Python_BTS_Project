ea9dfb10d3468ead046f25f9380758f81a6850ed
arisa94 <arisa9404@gmail.com>
2017-07-21 13:30:56 +0200

    Handling missing

--- algorithm.py
+++ algorithm.py
@@ -16,6 +16,7 @@ data = read_file()
 outputData = []
 terminals = {}
 missings = {}
+lastWorked = {}
 
 target = -75
 hysteresis = 3
@@ -39,14 +40,18 @@ def avg(numbers):
 
 def power_management():
     for i in data:
-        if i[3] == "missing":
+        missed = False
+        if i[3] == 1000:
+            missed = True
             if i[0] in ["UL", "DL"] and i[1] == "S0" and i[4] is None:
-                if missings[i[2]] <= missing:
-                    print()
-        elif(i[0] not in["UL", "DL"] or i[1] not in ["S0", "N1", "N2", "N3", "N4", "N5", "N6"] or i[3] < -95 or i[3] > -45\
+                if missings[i[0] + i[2]] <= missing:
+                    missings[i[0] + i[2]] += 1
+                    #TODO
+        if(i[0] not in["UL", "DL"] or i[1] not in ["S0", "N1", "N2", "N3", "N4", "N5", "N6"] or i[3] < -95 or i[3] > -45\
                or i[4] not in [0, 1, 2, 3, 4, 5]):
             outputData.append([0])
         elif i[1] == "S0":
+            missings[i[0] + i[2]] = 0
             if i[0] + i[2] in terminals:
                 terminals[i[0] + i[2]].append(i[3])
                 if len(terminals[i[0] + i[2]]) >= values:
@@ -80,7 +85,7 @@ def power_management():
             else:
                 terminals[i[0] + i[2]] = [i[3]]
                 outputData.append([i[0], i[1], i[2], "NCH"])
-
+            lastWorked[i[0] + i[2]] = i
 power_management()
 writeToTxt(outputData)
 #print(outputData)
