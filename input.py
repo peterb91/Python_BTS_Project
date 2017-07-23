25b69071f1726651c065cd129363282674566c31
peterb91 <piotrekbednarz@gmail.com>
2017-07-23 16:50:52 +0200

    read from stdin

--- input.py
+++ input.py
@@ -1,37 +1,37 @@
-#import sys
+import sys
 
 
 def read_file():
     sep = " "
     data = []
 
-    #content = sys.stdin.readlines()
-    with open("dataset.txt", encoding = 'utf-8') as file:
-        content = file.readlines()
-        for line in content:
-            line = line.strip()
-            l = line.split(sep)
-            if len(l) < 4:
-                add = [None for i in range(4 - len(l))]
-                l = l + add
-            for i in range(len(l)):
-                if l[i] == ('' or None) and i == 3:
-                    l[i] = 1000
-                elif l[i] == '' and i != 3:
-                    l[i] = None
-            if l[3] == "missing":
-                l[3] = 1000
-            if l[1] != "S0" and l[3] != 1000:
-                l[3] = int(l[3])
-                l.append(None)
-            elif l[1] == "S0" and l[3] != 1000:
-                l[3] = int(l[3])
-                l[4] = int(l[4])
-            elif l[3] == 1000:
-                l.append(None)
-            data.append(l),
-        for i in data:
-            print (i), "\n"
-        return data
+    content = sys.stdin.readlines()
+    #with open("dataset.txt", encoding = 'utf-8') as file:
+        #content = file.readlines()
+    for line in content:
+        line = line.strip()
+        l = line.split(sep)
+        if len(l) < 4:
+            add = [None for i in range(4 - len(l))]
+            l = l + add
+        for i in range(len(l)):
+            if l[i] == ('' or None) and i == 3:
+                l[i] = 1000
+            elif l[i] == '' and i != 3:
+                l[i] = None
+        if l[3] == "missing":
+            l[3] = 1000
+        if l[1] != "S0" and l[3] != 1000:
+            l[3] = int(l[3])
+            l.append(None)
+        elif l[1] == "S0" and l[3] != 1000:
+            l[3] = int(l[3])
+            l[4] = int(l[4])
+        elif l[3] == 1000:
+            l.append(None)
+        data.append(l),
+    #for i in data:
+        #print (i), "\n"
+    return data
 
 read_file()
