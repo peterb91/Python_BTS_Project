833e46c4276880c5dd04bcd4c8b2bd1afd772843
arisa94 <arisa9404@gmail.com>
2017-07-24 10:24:09 +0200

    Adding STDout

--- algorithm.py
+++ algorithm.py
@@ -61,30 +61,42 @@ def power_management():
                         if deviation < 0:
                             if abs(deviation) >= maxDec and i[4] < 2:
                                 outputData.append([i[0], i[1], i[2], "DEC", maxDec])
+                                print(i[0], i[1], i[2], "DEC", maxDec)
                             elif i[4] < 2:
                                 outputData.append([i[0], i[1], i[2], "DEC", -deviation])
+                                print(i[0], i[1], i[2], "DEC", -deviation)
                             else:
                                 outputData.append([i[0], i[1], i[2], "NCH", None])
+                                print(i[0], i[1], i[2], "NCH")
                         elif deviation > 0:
                             if abs(deviation) >= maxInc:
                                 outputData.append([i[0], i[1], i[2], "INC", maxInc])
+                                print(i[0], i[1], i[2], "INC", maxInc)
                             else:
                                 outputData.append([i[0], i[1], i[2], "INC", deviation])
+                                print(i[0], i[1], i[2], "INC", deviation)
                     elif abs(deviation) > hysteresis and i[4] >= 4:
                         if deviation > 2:
                             if abs(deviation) >= maxInc:
                                 outputData.append([i[0], i[1], i[2], "INC", maxInc])
+                                print(i[0], i[1], i[2], "INC", maxInc)
                             else:
                                 outputData.append([i[0], i[1], i[2], "INC", deviation])
+                                print(i[0], i[1], i[2], "INC", deviation)
                         else:
                             outputData.append([i[0], i[1], i[2], "INC", 2])
+                            print(i[0], i[1], i[2], "INC", 2)
                     else:
                         outputData.append([i[0], i[1], i[2], "NCH", None])
+                        print(i[0], i[1], i[2], "NCH")
                 else:
                     outputData.append([i[0], i[1], i[2], "NCH", None])
+                    print(i[0], i[1], i[2], "NCH")
             else:
                 terminals[i[0] + i[2]] = [i[3]]
-                outputData.append([i[0], i[1], i[2], "NCH", None])
+                if hysteresis > 1:
+                    outputData.append([i[0], i[1], i[2], "NCH", None])
+                    print(i[0], i[1], i[2], "NCH")
             lastWorked[i[0] + i[2]] = i[3]
     return outputData
 
