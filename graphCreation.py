e7c11e8ad0db5107bb2c869a28aa85fa71825972
Teresa Bury (UST <U58224@ust-global.com>
2017-07-24 14:52:10 +0200

    chart creation

---
+++ graphCreation.py
@@ -0,0 +1,26 @@
+import subprocess
+''' to run this function you have to have installed Gnuplot and Python 2
+    update lines 20 and 21 ('data1' and 'data2' with file name according to terminal name you want to see on graph'''
+
+
+def graphCreation():
+    proc = subprocess.Popen(["gnuplot","-p"],
+                            shell=True,
+                            stdin=subprocess.PIPE,
+                            )
+
+    proc.communicate("""
+    set title("input data for chosen MS identity")
+    set xlabel("Time [sec]")
+    set ylabel("Signal power [dB]")
+    set key top right
+    set key box
+    set key left bottom
+    set key bmargin
+    data1="ms_MS222.txt"
+    data2="missing_MS222.txt"
+    set terminal png size 400,300
+    set output "chart.png"
+    plot data1 with lines title "data", data2 title "missing data"
+    pause 4
+    """)
---
+++ inputCharts.py
@@ -0,0 +1,30 @@
+from input import read_file
+
+def inputCharts():
+    inData = read_file()
+    msNames = []
+    timestamp=0
+    # add timestamp to dataset
+    for i in inData:
+        i.append(timestamp)
+        timestamp+=0.5
+    # create list with unique MS terminal names
+    for i in range(len(inData)):
+        if inData[i][2] not in msNames:
+            msNames.append(inData[i][2])
+    # save data to separate txt files according to MS name
+    for ms in msNames:
+        with open("ms_" + ms + ".txt", "w") as file:
+            for i in inData:
+                if i[2] == ms and i[3]!=1000:
+                    file.write("%s %s" %(i[5],i[3]))
+                    file.write("\n")
+                else:
+                    file.write("\n")
+        # if there is some missing data save measurements in separate txt file
+        with open("missing_" + ms + ".txt", "w") as file:
+            for i in inData:
+                if i[2] == ms and i[3]==1000:
+                    file.write("%s %d" %(i[5],0))
+                    file.write("\n")
+
