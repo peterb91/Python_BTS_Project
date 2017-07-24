d8b6d75b5fb476f382ea781e2ea955f7e43fe4f5
peterb91 <piotrekbednarz@gmail.com>
2017-07-24 20:07:49 +0200

    comments added

--- data_generator.py
+++ data_generator.py
@@ -9,7 +9,7 @@ def random_data (amount):
         terminals = ["S0", "S0", "S0", "S0", "S0", "S0", "S0", "N1", "N2", "N3", "N4", "N5", "N6", "N7", "N8", "S1"]
         stations = ["MS776", "MS222", "MS543", "MS754"]
         strength = [i for i in range(-99, -95, 2)] + [i for i in range(-95, -45, 1)] + [i for i in range(-45, -10, 8)]\
-                   + ["missing"]
+                   + ["missing"]  # generates irreguar list of numbers from -99 to -10
         for i in range(amount):
             col1 = random.choice(links)
             col2 = random.choice(terminals)
