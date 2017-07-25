18159b20e3a7b85a75711b89c08823ab32b0e574
Teresa Bury (UST <U58224@ust-global.com>
2017-07-25 22:36:11 +0200

    adding logs

---
+++ logs.py
@@ -0,0 +1,6 @@
+import logging
+#import SaveOutputTxt
+#import algorithm
+
+logging.basicConfig(filename='myapp.log', format='%(asctime)s %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
